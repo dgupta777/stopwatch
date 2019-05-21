0c53fcd8c19a70e1fcdf3cd5b4d18cae028459cd
dgupta777 <darshitgupta.cse20@jecrc.ac.in>
2019-05-22 01:51:02 +0530

    add directly via upload

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+  counter: number;
+  timerRef;
+  running: boolean = false;
+  startText = 'Start';
+
+  constructor() { }
+
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      this.startText = 'Stop';
+      const startTime = Date.now() - (this.counter || 0);
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+      });
+    } else {
+      this.startText = 'Resume';
+      clearInterval(this.timerRef);
+    }
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = undefined;
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
+
+  ngOnInit() {
+  }
+
+}
